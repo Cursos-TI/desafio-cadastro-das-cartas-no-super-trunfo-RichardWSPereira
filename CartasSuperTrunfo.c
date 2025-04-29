6b142bfc16372b3796c45c3dd8747fc5fb8b19bf
RichardWSPereira <srichardwilliam@hotmail.com>
2025-04-29 00:05:01 +0000

    Especificação dos comandos

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,19 +3,30 @@ int main() {
 
     
     // Definação das variáveis separadas para cada atributo da cidade.
-    
+    // Estado, codigo, nome da cidade serão representados por letras, logo, serão caracteres, assim digita com "char" na frente do nome da variável.
+    // Diferentemente, o nome da cidade e codigo terão mais de um caractere, assim, é especificado entre colchetes a quantidade de caracteres.
+
     char estadoA, estadoB;
     char codigoA[3], codigoB[3];
     char nomedacidadeA[30], nomedacidadeB[30];
-    int populacaoA, pontosturisticoA, populacaoB, pontosturisticoB;
-    float areaA, pibA, areaB, pibB;
 
-    // Cadastro das Cartas:  
+    // Na população é digitado um número inteiro, logo, colocamos o "int" para atribuir a variável este atributo, depois colocamos os nomes das variável separadas com vírgula
+    int populacaoA, pontosturisticoA, populacaoB, pontosturisticoB;
+
+    // Já o PIB e área é um ponto fluante, números que podem ser decimais, assim, "float" faz está definição antes de digitar os nomes das variáveis.
+    float areaA, pibA, areaB, pibB;
     
     printf("Desafio Super Trunfo - Países \n");
     printf("Tema 1 - Cadastro das Cartas \n");
     printf("Teste Richard \n");
     printf("                               \n");
+
+    // O printf é o comando que imprime na tela os dizeres
+    // Já o scanf solicita ao usuário para digitar o que foi pedido 
+    /* As variaveis tem os especificadores que são acompanhados pela porcentagens antes de escrevê-los para se remeter a alguma variavel,
+    logo: %c (caractere) ; %s (strings) ; %f (numeros flutuantesna notação padrão);
+    Logo depois, colocamos a variável que desejamos gravar o que o usuário digitou, lembrando de colocar o "&" antes de especificar a variável*/
+    // Cadastro das Cartas:  
 
     printf("Qual é a letra que representa este estado: ");
     scanf("%c",&estadoA);
@@ -31,10 +42,12 @@ int main() {
     scanf("%f",&pibA);
     printf("Qual é a quantidade de Pontos Turísticos: ");
     scanf("%d", &pontosturisticoA);
-    
+
     printf("                               \n");
     printf("--------- Novo cadastro --------\n");
     printf("                               \n");
+
+    // Foi utilizado o /n para pular a linha na execução do próximo código, assim como foi usado o printf com sucessivos espaços ṕara haver uma linha em branco entre os dizeres na tela
 
     printf("Qual é a letra que representa este novo estado: ");
     scanf("%s",&estadoB);
