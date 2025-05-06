88c9f4d08c98293fb064b09291721b2fc76aa58c
RichardWSPereira <srichardwilliam@hotmail.com>
2025-05-06 00:02:07 +0000

    Primeira parte do nivel mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,15 +11,21 @@ int main() {
     // Na população é digitado um número inteiro, logo, colocamos o "int" para atribuir a variável este atributo, depois colocamos os nomes das variável separadas com vírgula
     // Já o PIB e área é um ponto fluante, números que podem ser decimais, assim, "float" faz está definição antes de digitar os nomes das variáveis.
     
-    char estB, estadoA;
-    char codeB[3], codigoA[3], nomedacidadeB[30], nomedacidadeA[30];   
-    int populacaoA, pontosturisticoA, populacaoB, pontosturisticoB;  
-    float areaA, pibA, areaB, pibB;
+    char estB = "Z", estadoA = "z";
+    char codeB[3] = "Cod", codigoA[3] = "CoB", nomedacidadeB[30] = "nomeA", nomedacidadeA[30] = "nomeB";   
+    unsigned long int populacaoA = 0,populacaoB = 0;
+    int pontosturisticoA = 0,  pontosturisticoB = 0;  
+    float areaA = 0, pibA = 0, areaB = 0, pibB = 0;
 
     // Criarei as variáveis de Densidade Populacional e PIB per capita, sendo estas variáveis do tipo float
     
-    float densidadepopA, PIBpercapitaA;
-    float densidadepopB, PIBpercapitaB;
+    float densidadepopA = 0, PIBpercapitaA = 0;
+    float densidadepopB = 0, PIBpercapitaB = 0;
+
+    // O Super Poder também será declarado como float, pois será o resultado do somatório de todos os atributos,
+    // Atributos que contêm variáveis com casas decimais
+
+    float SuperPoderA, SuperPoderB;
 
     printf("Desafio Super Trunfo - Países \n");
     printf("Tema 1 - Cadastro das Cartas \n");
@@ -53,10 +59,13 @@ int main() {
     // Como também, devo dividir o PIB da cidade pela população para encontrar o PIB per capita
     // Esta conta sendo feito já dentro da atribuição das variáveis criadas para Densidade populacional e PIB per capita
     // Para que não haja problema com conversão de inteiros para float, vou explicitamente atribuir como float
+    // Agora realizarei a conta do Super Poder, somando todos os atributos
+    // Prestando atenção nas variaveis de tipos diferentes
 
     densidadepopA = (float) populacaoA / areaA;
     PIBpercapitaA = pibA / (float) populacaoA;
-
+    SuperPoderA = ((float) populacaoA + areaA + pibA + (float) pontosturisticoA + 1/densidadepopA + PIBpercapitaA);
+    
     // Exibição dos Dados da Carta 1:
 
     printf("* Carta 1 *\n");
@@ -69,6 +78,8 @@ int main() {
     printf("Número de Pontos Turísticos: %d \n", pontosturisticoA);
     printf("Densidade Populacional: %.2f hab/km² \n", densidadepopA);
     printf("PIB per Capita: %.2f reais \n", PIBpercapitaA);
+    // Irei imprimir o valor do Super Poder de cada carta para saber se a conta foi realizada corretamente
+    printf("O valor do Super Poder é: %.2f \n", SuperPoderA);
 
     printf("                               \n");
     printf("--------- Novo cadastro --------\n");
@@ -93,10 +104,11 @@ int main() {
     scanf("%d", &pontosturisticoB);
     printf("                               \n");
 
-    // Mesmo processo para descobrir a densidade populacional e PIB per capita da cidade 1 será feito nesta
+    // Mesmo processo para descobrir a densidade populacional, PIB per capita e Super poder da cidade 1 será feito nesta
 
     densidadepopB = (float) populacaoB / areaB;
     PIBpercapitaB = pibB / (float) populacaoB;
+    SuperPoderB = ((float) populacaoB + areaB + pibB + (float) pontosturisticoB + 1/densidadepopB + PIBpercapitaB);
 
     printf("* Carta 2 *\n");
     printf("Estado: %c \n", estB);
@@ -108,6 +120,7 @@ int main() {
     printf("Número de Pontos Turísticos: %d \n", pontosturisticoB);
     printf("Densidade Populacional: %.2f hab/km² \n", densidadepopB);
     printf("PIB per Capita: %.2f reais \n", PIBpercapitaB);
+    printf("O valor do Super Poder é: %.2f \n", SuperPoderB);
     printf("------------------------------\n");
 
     return 0;
