a5509f48c1e4638a16f7a6b2017c7777166b757a
RichardWSPereira <srichardwilliam@hotmail.com>
2025-05-07 23:20:30 +0000

    If else - Nível Novato no desenvolvimento da lógica

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 
 int main() {
@@ -11,11 +12,10 @@ int main() {
     // Na população é digitado um número inteiro, logo, colocamos o "int" para atribuir a variável este atributo, depois colocamos os nomes das variável separadas com vírgula
     // Já o PIB e área é um ponto fluante, números que podem ser decimais, assim, "float" faz está definição antes de digitar os nomes das variáveis.
     
-    char estB, estadoA;
-    char codeB[3] = "Cod", codigoA[3] = "CoB", nomedacidadeB[30] = "nomeA", nomedacidadeA[30] = "nomeB";   
-    unsigned long int populacaoA = 0,populacaoB = 0;
-    int pontosturisticoA = 0,  pontosturisticoB = 0;  
-    float areaA = 0, pibA = 0, areaB = 0, pibB = 0;
+    char estadoB[50], estadoA[50], codigoB[3], codigoA[3], nomedacidadeB[50], nomedacidadeA[50];   
+    unsigned long int populacaoA,populacaoB;
+    int pontosturisticoA,  pontosturisticoB;  
+    float areaA, pibA, areaB, pibB;
 
     // Criarei as variáveis de Densidade Populacional e PIB per capita, sendo estas variáveis do tipo float
     
@@ -38,10 +38,10 @@ int main() {
     logo: %c (caractere) ; %s (strings) ; %f (numeros flutuantesna notação padrão);
     Logo depois, colocamos a variável que desejamos gravar o que o usuário digitou, lembrando de colocar o "&" antes de especificar a variável*/
     // Cadastro das Cartas:  
-
-    printf("Qual é a letra que representa este estado: ");
-    scanf("%c",&estadoA);
-    printf("Qual é o código da cidade do estado %c: ", estadoA);
+    
+    printf("Qual é este estado: "); 
+    scanf("%s",estadoA);
+    printf("Qual é o código da cidade do estado %s: ",estadoA);
     scanf("%s",codigoA);
     printf("Qual o nome da cidade: ");
     scanf("%s",nomedacidadeA);
@@ -49,7 +49,7 @@ int main() {
     scanf("%lu",&populacaoA);
     printf("Qual é a área em km2: ");
     scanf("%f",&areaA);
-    printf("Quantos bilhões tem o pib desta cidade: ");
+    printf("Quantos bilhões tem o PIB desta cidade: ");
     scanf("%f",&pibA);
     printf("Qual é a quantidade de Pontos Turísticos: ");
     scanf("%d", &pontosturisticoA);
@@ -69,7 +69,7 @@ int main() {
     // Exibição dos Dados da Carta 1:
 
     printf("* Carta 1 *\n");
-    printf("Estado: %c \n",estadoA);
+    printf("Estado: %s \n",estadoA);
     printf("Código: %s \n", codigoA);
     printf("Nome da Cidade: %s \n", nomedacidadeA);
     printf("População: %lu \n", populacaoA);
@@ -88,17 +88,17 @@ int main() {
     // Foi utilizado o /n para pular a linha na execução do próximo código, assim como foi usado o printf com sucessivos espaços ṕara haver uma linha em branco entre os dizeres na tela
     // Podemos atribuir quantas casas decimais queremos nos pontos flutuantes, basta coloca o ponto e o numero exemplo o que foi colocado no codigo em área e PIB: %.1f
 
-    printf("Qual é a letra que representa este novo estado: "); 
-    scanf("%s",&estB);
-    printf("Qual é o código da cidade do estado %c: ",estB);
-    scanf("%s",codeB);
+    printf("Qual é este novo estado: "); 
+    scanf("%s",estadoB);
+    printf("Qual é o código da cidade do estado %s: ",estadoB);
+    scanf("%s",codigoB);
     printf("Qual o nome da cidade: ");
     scanf("%s",nomedacidadeB);
     printf("Qual é o número de habitantes: ");
     scanf("%lu",&populacaoB);
     printf("Qual é a área em km2: ");
     scanf("%f",&areaB);
-    printf("Quantos bilhões tem o pib desta cidade: ");
+    printf("Quantos bilhões tem o PIB desta cidade: ");
     scanf("%f",&pibB);
     printf("Qual é a quantidade de Pontos Turísticos: ");
     scanf("%d", &pontosturisticoB);
@@ -111,8 +111,8 @@ int main() {
     SuperPoderB = ((float) populacaoB + areaB + pibB + (float) pontosturisticoB + 1/densidadepopB + PIBpercapitaB);
 
     printf("* Carta 2 *\n");
-    printf("Estado: %c \n", estB);
-    printf("Código: %s \n", codeB);
+    printf("Estado: %s \n", estadoB);
+    printf("Código: %s \n", codigoB);
     printf("Nome da Cidade: %s \n", nomedacidadeB);
     printf("População: %lu \n", populacaoB);
     printf("Área: %.1f km2 \n", areaB);
@@ -121,25 +121,27 @@ int main() {
     printf("Densidade Populacional: %.2f hab/km² \n", densidadepopB);
     printf("PIB per Capita: %.2f reais \n", PIBpercapitaB);
     printf("O valor do Super Poder é: %.2f \n", SuperPoderB);
-    printf("---------------------------------------\n");
     printf("                               \n");
 
     // Agora vamos realizar as comparações dos atributos carta por carta
     // Dessa forma, usando os operadores relacionais, receberei como resultado o valor 0 para falso, quando algum atributo não é maior que o outro e resultado 1 para verdadeiro
-    
-    printf("--------- Comparação de cartas --------\n");
-    printf("=> 1 = verdadeiro \n");
-    printf("=> 0 = falso \n");
-    printf("                               \n");
-    printf("População da carta 1 é maior que o da carta 2: %d \n", populacaoA > populacaoB);
+    /* printf("População da carta 1 é maior que o da carta 2: %d \n", populacaoA > populacaoB);
     printf("Área da carta 1 é maior que o da carta 2: %d \n", areaA > areaB);
     printf("PIB da carta 1 é maior que o da carta 2: %d \n", pibA > pibB);
     printf("Números de Pontos Turísticos da carta 1 é maior que o da carta 2: %d \n", pontosturisticoA > pontosturisticoB);
     printf("Densidade Populacional da carta 1 é menor que o da carta 2: %d \n", densidadepopA < densidadepopB);
     printf("PIB Per Capitada carta 1 é maior que o da carta 2: %d \n", PIBpercapitaA > PIBpercapitaB);
-    printf("Super Poder da carta 1 é maior que o da carta 2: %d \n", SuperPoderA > SuperPoderB);
+    printf("Super Poder da carta 1 é maior que o da carta 2: %d \n", SuperPoderA > SuperPoderB); */
 
-    
+    printf("--------- Comparação de cartas  (Atributo: População): --------\n");
+    printf("                               \n");
+    printf("Carta 1 - %s (%s): %f \n", nomedacidadeA, estadoA, densidadepopA);
+    printf("Carta 1 - %s (%s): %f \n", nomedacidadeB, estadoB, densidadepopB);
+        if (densidadepopA < densidadepopB) {
+            printf("Resultado: Carta 1 (%s) venceu! \n", nomedacidadeA);
+        } else {
+            printf("Resultado: Carta 2 (%s) venceu! \n", nomedacidadeB);
+        }
 
 
     return 0;
